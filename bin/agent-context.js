b378ec05b24388e7ff186d62c0f0fb06da96a795
Arham-Qureshi <arhamqureshi0098@gmail.com>
2026-07-08 12:35:17 +0530

    add: query and path commands

--- bin/agent-context.js
+++ bin/agent-context.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import { initCommand, generateCommand, cleanCommand, serveCommand } from '../src/cli/commands.js';
+import { initCommand, generateCommand, cleanCommand, serveCommand, queryCommand, pathCommand } from '../src/cli/commands/index.js';
 
 const program = new Command();
 
 program
   .name('agent-context')
-  .version('1.0.0')
+  .version('1.1.0')
   .description('A local CLI tool that parses codebases, builds dependency graphs, and outputs interactive architecture visualizations.');
 
 program
@@ -31,5 +31,15 @@ program
   .description('Start a local web server to view the generated graph')
   .option('-p, --port <number>', 'Port to run on', '3000')
   .action(serveCommand);
+
+program
+  .command('query <target>')
+  .description('Look up a node\'s dependencies and dependents from the generated graph')
+  .action(queryCommand);
+
+program
+  .command('path <source> <target>')
+  .description('Trace the shortest dependency path between two nodes in the graph')
+  .action(pathCommand);
 
 program.parse(process.argv);
