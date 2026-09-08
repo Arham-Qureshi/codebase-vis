63e57d9eef78726950c5314698e9caece6036e2e
Arham-Qureshi <arhamqureshi0098@gmail.com>
2026-09-09 00:18:50 +0530

    parser: add image asset extractor for referenced files

---
+++ src/parser/image.js
@@ -0,0 +1,9 @@
+export const grammar = null;
+
+export function extractDependencies() {
+  return [];
+}
+
+export function extractEntities() {
+  return { classes: [], functions: [], methods: [], docstrings: [] };
+}
