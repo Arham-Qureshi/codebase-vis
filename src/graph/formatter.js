ad624dd23ba9cf2bcd3b7fe611aa54eb065c5db1
Arham-Qureshi <arhamqureshi0098@gmail.com>
2026-09-15 00:16:46 +0530

    perf: streaming JSON export with dead attribute stripping

--- src/graph/formatter.js
+++ src/graph/formatter.js
@@ -4,27 +4,45 @@ import { safeWriteFile } from '../utils/file-system.js';
 const STRIP_NODE_KEYS = new Set(['x', 'y', 'size', 'dependencies', 'depth', 'parent', 'scriptName']);
 const STRIP_EDGE_KEYS = new Set(['depth', 'linkText']);
 
-function stripDeadAttributes(graphData) {
-  for (const node of graphData.nodes) {
-    if (!node.attributes) continue;
-    for (const key of STRIP_NODE_KEYS) {
-      if (key in node.attributes) delete node.attributes[key];
-    }
+function stripAttrs(attrs, stripSet) {
+  const clean = {};
+  for (const [k, v] of Object.entries(attrs)) {
+    if (!stripSet.has(k)) clean[k] = v;
   }
-  for (const edge of graphData.edges) {
-    if (!edge.attributes) continue;
-    for (const key of STRIP_EDGE_KEYS) {
-      if (key in edge.attributes) delete edge.attributes[key];
-    }
-  }
-  return graphData;
+  return clean;
 }
 
 export async function exportGraphToJson(graph, outDir) {
-  const graphData = stripDeadAttributes(graph.export());
-  const usePretty = graphData.nodes.length < 50000;
-  const data = JSON.stringify(graphData, null, usePretty ? 2 : 0);
   const targetPath = path.join(outDir, 'graph.json');
-  await safeWriteFile(targetPath, data);
+  const usePretty = graph.order < 50000;
+  const nl = usePretty ? '\n' : '';
+  const sp = usePretty ? '  ' : '';
+
+  const options = { type: graph.type, multi: graph.multi };
+  const chunks = [];
+  chunks.push('{"options":' + JSON.stringify(options) + ',' + nl);
+  chunks.push('"nodes":[' + nl);
+
+  let nodeIdx = 0;
+  graph.forEachNode((key, attributes) => {
+    const clean = stripAttrs(attributes || {}, STRIP_NODE_KEYS);
+    const prefix = nodeIdx > 0 ? ',' + nl : '';
+    chunks.push(prefix + sp + JSON.stringify({ key, attributes: clean }));
+    nodeIdx++;
+  });
+
+  chunks.push(nl + '],' + nl + '"edges":[' + nl);
+
+  let edgeIdx = 0;
+  graph.forEachEdge((edge, attributes, source, target) => {
+    const clean = stripAttrs(attributes || {}, STRIP_EDGE_KEYS);
+    const prefix = edgeIdx > 0 ? ',' + nl : '';
+    chunks.push(prefix + sp + JSON.stringify({ source, target, attributes: clean }));
+    edgeIdx++;
+  });
+
+  chunks.push(nl + ']}');
+
+  await safeWriteFile(targetPath, chunks.join(''));
   return targetPath;
 }
